968c8e93d8a33328e98c77c6946464f2cc57c373
权权 <qzz205611@163.com>
2026-05-25 00:47:49 -0700

    纠错

--- 06_ensemble-learning/04_xgboost/main.py
+++ 06_ensemble-learning/04_xgboost/main.py
@@ -9,14 +9,14 @@ from sklearn.utils import class_weight
 test_data = pd.read_csv("./datasets/红酒品质分类_test.csv")
 train_data = pd.read_csv("./datasets/红酒品质分类_train.csv")
 X_train,y_train = train_data.iloc[:, :-1],train_data.iloc[:, -1]
-X_test,y_test = test_data.iloc[:, :-1],test_data.iloc[: -1]
+X_test,y_test = test_data.iloc[:, :-1],test_data.iloc[:, -1]
 
 # 加载预训练模型
 estimator = joblib.load("./datasets/红酒品质分类.pkl")
 
 ## 网格搜索 + 分层 K 折
 param_dict = { "max_depth" :[1,2,3,4,5],
-              "n_estimator":[100,200,300,400,500],
+              "n_estimators":[100,200,300,400,500],
               "learning_rate" :[0.1,0.2,0.3,0.4,0.5]
             
 }
@@ -28,4 +28,4 @@ gs.fit(X_train,y_train)
 print(f"最优模型为：{gs.best_params_}")
 print(f"最优CV评分为:{gs.best_score_}")
 y_pred = gs.predict(X_test)
-print(f"测试集准确率:{accuracy_score(X_test,y_pred)}")
+print(f"测试集准确率:{accuracy_score(y_test,y_pred)}")
